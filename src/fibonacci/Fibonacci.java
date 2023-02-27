8a9621ed607b58a010705884dc567db5de50f878
Nikta Zemlianskikh <rymzar@gmail.com>
2023-02-27 13:21:23 +0300

    first commit

---
+++ src/fibonacci/Fibonacci.java
@@ -0,0 +1,28 @@
+package fibonacci;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fibNiave(3));
+        System.out.println(fibNiave(5));
+        System.out.println(fibEffectiv(40));
+    }
+    //наивный, медленный, очевидный
+    private static long fibNiave(int n) {
+        if (n <= 1) return n;
+
+        return fibNiave(n - 1) + fibNiave(n - 2);
+    }
+    //задействовав доп память, сокращаем время работы
+    private static long fibEffectiv(int n) {
+        long[] arr = new long[n + 1];
+
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+
+        return arr[n];
+    }
+}
